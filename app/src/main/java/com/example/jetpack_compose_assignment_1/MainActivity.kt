66482ab63c0661cf6b99ea6a6a9591802323ee74
Kidxcudi <kidxcudi2@gmail.com>
2025-05-11 15:40:08 +0300

    State in Compose

--- app/src/main/java/com/example/jetpack_compose_assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_assignment_1/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -37,21 +39,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(Modifier.padding(24.dp)){
-            Column(modifier = Modifier.weight(1f)
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)
                 ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
 
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value  }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
